ea6ec7c75f60d5dbd9781db06e091b4880eff893
M.Yazid <m.yazid@MacBook-Pro-MYazid.local>
2023-06-14 14:59:42 +0700

    main:fix route error

--- src/router/RouterManagement.jsx
+++ src/router/RouterManagement.jsx
@@ -1,12 +1,12 @@
 import React, { Suspense, useEffect } from "react";
 import { Route, Routes, useNavigate, Navigate } from "react-router-dom";
-import LandingMain from "../pages/landingPage/LandingMain";
+import RiwayatPresensi from "../pages/riwayatPresensi/RiwayatPresensi";
 import LoginAdmin from "../pages/loginAdmin/LoginAdmin";
 import LayoutComponent from "../components/layout/LayoutComponent";
-import DashboardAdmin from "../pages/dashboradAdmin/DashboardAdmin";
+import LandingMain from "../pages/landingPage/LandingMain";
+import DashboardAdmin from "../pages/dashboardAdmin/DashboardAdmin";
 import JadwalKuliahAdmin from "../pages/jadwalKuliahAdmin/JadwalKuliahAdmin";
 import DataMahasiswa from "../pages/dataMahasiswa/DataMahasiswa";
-import RiwayatPresensi from "../pages/riwayatPresensi/RiwayatPresensi";
 
 const RouterManagement = () => {
   const token = localStorage.getItem("access_token");
