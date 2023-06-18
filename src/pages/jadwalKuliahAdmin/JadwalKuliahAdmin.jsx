708769919d7ee7890bc300d42a795cfe1da39b95
myazid13 <messi.yazid10@gmail.com>
2023-06-18 21:33:11 +0700

    main:fix error

--- src/pages/jadwalKuliahAdmin/JadwalKuliahAdmin.jsx
+++ src/pages/jadwalKuliahAdmin/JadwalKuliahAdmin.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Table } from "antd";
-import "./JadwalKuliah.css";
+import "./jadwalKuliah.css";
 import { Link } from "react-router-dom";
 
 const dataSource = [
