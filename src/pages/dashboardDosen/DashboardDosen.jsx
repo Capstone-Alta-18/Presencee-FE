998dead13c1b38e9fa16436ab233cf93115c80f4
Rezkianwar <123230737+Rezkianwar@users.noreply.github.com>
2023-06-21 16:36:11 +0700

    feat/dashboard-dosen : add DashboardDosen.jsx dan DashboardDosen.css

--- src/pages/dashboardDosen/DashboardDosen.jsx
+++ src/pages/dashboardDosen/DashboardDosen.jsx
@@ -1,7 +1,39 @@
+import { Calendar, Col, Row } from "antd";
 import React from "react";
+import "./DashboardDosen.css";
+
+function formatDate(date) {
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+  return date.toLocaleDateString("en-US", options);
+}
 
 const DashboardDosen = () => {
-  return <div>DashboardDosen</div>;
+  const today = new Date();
+  const formattedDate = formatDate(today);
+
+  return (
+    <div className="dashboard-dosen-section">
+      <div className="title-dashboard-dosen">
+        <h2>Selamat datang, Dosen!</h2>
+        <p>{formattedDate}</p>
+      </div>
+      <Row>
+        <Col className="left-dashboard-dosen">
+          <div className="calender-dosen">
+            <Calendar fullscreen={false} />
+          </div>
+          <div className="presentase-kehadiran">
+            <h3>Presentase Kehadiran</h3>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default DashboardDosen;
