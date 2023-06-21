cf79143dcd3bff3954b7a25e30cbe7d77e53507f
Rezkianwar <123230737+Rezkianwar@users.noreply.github.com>
2023-06-21 19:09:21 +0700

    feat/dashboard-dosen : update DashboardDosen.jsx and DashboardDosen.css

--- src/pages/dashboardDosen/DashboardDosen.jsx
+++ src/pages/dashboardDosen/DashboardDosen.jsx
@@ -1,5 +1,6 @@
 import { Calendar, Col, Row } from "antd";
 import React from "react";
+import { IMAGES } from "../../assets/constant";
 import "./DashboardDosen.css";
 
 function formatDate(date) {
@@ -16,19 +17,44 @@ const DashboardDosen = () => {
   const today = new Date();
   const formattedDate = formatDate(today);
 
+  const renderHeader = ({ value, type, onChange }) => {
+    const yearText = value.format("YYYY");
+    const headerText = type === "month" ? value.format(" ddd, DD MMMM") : value.format("D MMMM YYYY");
+    return (
+      <div className="style-calendar">
+        <div>{yearText}</div>
+        <div>{headerText}</div>
+      </div>
+    );
+  };
+
   return (
     <div className="dashboard-dosen-section">
       <div className="title-dashboard-dosen">
-        <h2>Selamat datang, Dosen!</h2>
+        <h1>Selamat datang, Dosen!</h1>
         <p>{formattedDate}</p>
       </div>
       <Row>
         <Col className="left-dashboard-dosen">
           <div className="calender-dosen">
-            <Calendar fullscreen={false} />
+            <Calendar fullscreen={false} headerRender={renderHeader} />
           </div>
-          <div className="presentase-kehadiran">
-            <h3>Presentase Kehadiran</h3>
+          <div className="total-kehadiran-mahasiswa">
+            <h1>Total Kehadiran Mahasiswa</h1>
+          </div>
+        </Col>
+        <Col className="right-dashboard-dosen">
+          <div className="konfirmasi-kehadiran">
+            <img className="img-vector-konfirmasi-kehadiran" src={IMAGES.Vector} alt="vector" />
+            <div className="content-konfirmasi-kehadiran">
+              <h3>Kehadiran Terkonfirmasi</h3>
+            </div>
+          </div>
+          <div className="konfirmasi-kehadiran">
+            <img className="img-vector-konfirmasi-on-progres" src={IMAGES.Vector2} alt="vector" />
+            <div className="content-konfirmasi-kehadiran">
+              <h3>Konfirmasi On Proses</h3>
+            </div>
           </div>
         </Col>
       </Row>
